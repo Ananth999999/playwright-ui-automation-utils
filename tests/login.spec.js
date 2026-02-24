de0b869d91f136f7e4b89f944ad0d75fdca2264e
Ananth Mohan Ananthula <118047590+Ananth999999@users.noreply.github.com>
2026-02-23 23:22:48 -0500

    Add Playwright login test

---
+++ tests/login.spec.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@playwright/test');
+
+test('SauceDemo login works', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page).toHaveURL(/inventory/);
+});
+
